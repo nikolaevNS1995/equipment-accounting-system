82c5d98571f981f016d649edebe82782a4583060
NikolaevNS1995 <nikolaevns1995@gmail.com>
2025-01-20 18:02:29 +0300

    Созданы миграции, модели, контроллеры и реквесты

---
+++ app/Http/Controllers/EquipmentBrendController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EquipmentBrend\StoreEquipmentBrendRequest;
+use App\Http\Requests\EquipmentBrend\UpdateEquipmentBrendRequest;
+use App\Models\EquipmentBrend;
+
+class EquipmentBrendController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEquipmentBrendRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EquipmentBrend $equipmentBrend)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEquipmentBrendRequest $request, EquipmentBrend $equipmentBrend)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EquipmentBrend $equipmentBrend)
+    {
+        //
+    }
+}
