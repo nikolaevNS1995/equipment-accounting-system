df0fbdd41362b5e374e58cae280d0a251f4e3cb8
NikolaevNS1995 <nikolaevns1995@gmail.com>
2025-03-04 17:04:22 +0300

    updated EquipmentTypeController, StoreEquipmentTypeRequest, UpdateEquipmentTypeRequest and added Resources

--- app/Http/Controllers/EquipmentTypeController.php
+++ app/Http/Controllers/EquipmentTypeController.php
@@ -2,49 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreEquipmentTypeRequest;
-use App\Http\Requests\UpdateEquipmentTypeRequest;
+use App\Http\Requests\EquipmentType\StoreEquipmentTypeRequest;
+use App\Http\Requests\EquipmentType\UpdateEquipmentTypeRequest;
+use App\Http\Resources\EquipmentType\IndexResource;
+use App\Http\Resources\EquipmentType\ShowResource;
 use App\Models\EquipmentType;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class EquipmentTypeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        //
+        $equipmentTypes = EquipmentType::get();
+        return IndexResource::collection($equipmentTypes);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreEquipmentTypeRequest $request)
+    public function store(StoreEquipmentTypeRequest $request): ShowResource
     {
-        //
+        $data = $request->validated();
+        $equipmentType = EquipmentType::create($data);
+        return new ShowResource($equipmentType);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(EquipmentType $equipmentType)
+    public function show(EquipmentType $equipmentType): ShowResource
     {
-        //
+        return new ShowResource($equipmentType);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateEquipmentTypeRequest $request, EquipmentType $equipmentType)
+    public function update(UpdateEquipmentTypeRequest $request, EquipmentType $equipmentType): ShowResource
     {
-        //
+        $data = $request->validated();
+        $equipmentType->update($data);
+        return new ShowResource($equipmentType);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(EquipmentType $equipmentType)
+    public function destroy(EquipmentType $equipmentType): Response
     {
-        //
+        $equipmentType->delete();
+        return response()->noContent();
     }
 }
