871c05eccecad1fcd771885b12bb8b754a4ec833
NikolaevNS1995 <nikolaevns1995@gmail.com>
2025-03-04 21:03:28 +0300

    updated api routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,25 @@
 <?php
 
 use App\Http\Controllers\BuildingController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\CabinetController;
+use App\Http\Controllers\CabinetTypeController;
+use App\Http\Controllers\EquipmentBrandController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\EquipmentModelController;
+use App\Http\Controllers\EquipmentTypeController;
+use App\Http\Controllers\FurnitureController;
+use App\Http\Controllers\FurnitureTypeController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('buildings', BuildingController::class);
+Route::apiResource('cabinets', CabinetController::class);
+Route::apiResource('cabinet-types', CabinetTypeController::class);
+Route::apiResource('equipment-brands', EquipmentBrandController::class);
+Route::apiResource('equipment-models', EquipmentModelController::class);
+Route::apiResource('equipment-types', EquipmentTypeController::class);
+Route::apiResource('equipments', EquipmentController::class);
+Route::apiResource('furniture-types', FurnitureTypeController::class);
+Route::apiResource('furnitures', FurnitureController::class);
+Route::apiResource('roles', RoleController::class);
 
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
