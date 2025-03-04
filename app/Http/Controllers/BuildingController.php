37bbe8530590c68c7bebdd9554f2b1cac772e3d6
NikolaevNS1995 <nikolaevns1995@gmail.com>
2025-03-04 13:24:49 +0300

    updated BuildingController, Requests and added Resouce

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -2,49 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreBuildingRequest;
-use App\Http\Requests\UpdateBuildingRequest;
+use App\Http\Requests\Building\StoreBuildingRequest;
+use App\Http\Requests\Building\UpdateBuildingRequest;
+use App\Http\Resources\Building\IndexResource;
+use App\Http\Resources\Building\ShowResource;
 use App\Models\Building;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class BuildingController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        //
+        $buildings = Building::get();
+        return IndexResource::collection($buildings);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreBuildingRequest $request)
+    public function store(StoreBuildingRequest $request): ShowResource
     {
-        //
+        $data = $request->validated();
+        $building = Building::create($data);
+
+        return new ShowResource($building);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Building $building)
+    public function show(Building $building): ShowResource
     {
-        //
+        return new ShowResource($building);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateBuildingRequest $request, Building $building)
+    public function update(UpdateBuildingRequest $request, Building $building): ShowResource
     {
-        //
+        $data = $request->validated();
+        $building->update($data);
+        return new ShowResource($building);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Building $building)
+    public function destroy(Building $building): Response
     {
-        //
+        $building->delete();
+        return response()->noContent();
     }
 }
